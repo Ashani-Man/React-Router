1bf6871ccb8d58e740251d1f18cac9195fb108e2
Ashani M <ashanim@Ashanis-Air.home>
2024-04-04 15:36:32 -0400

    Successfully Linked Routes and changed css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import './App.css'
 import blue from "./Blue"
 import red from "./Red"
+import homePage from "./home"
 function App() {
 
   return (
@@ -10,11 +11,13 @@ function App() {
       <div id="navbar">
         <Link to="/blue">Blue</Link>
         <Link to="/red">Red</Link>
+        <Link to ="/home"> Home</Link>
       </div>
       <div id="main-section">
         <Routes>
           <Route path="/blue" element = {blue()}/>
           <Route path="/red" element={red()} />
+           <Route path="/home" element ={homePage()}></Route>
         </Routes>       
         </div>
     </div>
---
+++ src/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const homePage = () => {
+
+  return <div className="home"> Welcome Home</div>
+
+}
+
+export default homePage;
